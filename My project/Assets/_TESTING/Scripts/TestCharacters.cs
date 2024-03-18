9470b0875c329abdaf9083d67c065a32b5c10aeb
IvacsonIngrid <ivacson.ingrid@student.ms.sapientia.ro>
2024-03-18 21:47:33 +0300

    Game v1.4
    Characters based on photo (Reallusion), show/hide character

--- My project/Assets/_TESTING/Scripts/TestCharacters.cs
+++ My project/Assets/_TESTING/Scripts/TestCharacters.cs
@@ -11,17 +11,56 @@ namespace TESTING
     {
         public TMP_FontAsset tempFont;
 
+        private Character CreateCharacter(string name) => CharacterManager.instance.CreateCharacter(name);
+
         // Start is called before the first frame update
         void Start()
         {
             //Character Melory = CharacterManager.instance.CreateCharacter("Melory");
             //Character Demian = CharacterManager.instance.CreateCharacter("Demian");
             //Character Demian2 = CharacterManager.instance.CreateCharacter("Demian");
-            //Character Adam = CharacterManager.instance.CreateCharacter("Adam");
+            //Character Adam = CharacterManager.instance.CreateCharacter("Livia");
             StartCoroutine(Test());
         }
 
+        //masodik proba
         IEnumerator Test()
+        {
+            Character guard1 = CreateCharacter("Guard1 as Generic");
+            Character guard2 = CreateCharacter("Guard2 as Generic");
+            Character guard3 = CreateCharacter("Guard3 as Generic");
+
+            guard1.Show();
+            guard2.Show();
+            guard3.Show();
+
+            guard1.SetDialogueFont(tempFont);
+            guard1.SetNameFont(tempFont);
+            guard2.SetDialogueColor(Color.cyan);
+            guard3.SetNameColor(Color.red);
+
+            yield return guard1.Say("It is important");
+            yield return guard2.Say("What happend?");
+            yield return guard3.Say("I don't know...");
+
+            yield return null;
+        }
+
+        //masodik proba
+        /*IEnumerator Test()
+        {
+            yield return new WaitForSeconds(1f); 
+            Character Melory = CharacterManager.instance.CreateCharacter("Melory");
+
+            yield return new WaitForSeconds(1f);
+            yield return Melory.Hide();
+            yield return new WaitForSeconds(0.5f);
+            yield return Melory.Show();
+            yield return Melory.Say("Hello!");
+        }*/
+
+        //elso proba
+        /*IEnumerator Test()
         {
             Character Melory = CharacterManager.instance.CreateCharacter("Melory");
             Character fifi = CharacterManager.instance.CreateCharacter("f");
@@ -71,7 +110,7 @@ namespace TESTING
             yield return Adam.Say("This is just one line. {a}It is a simple line.");
 
             Debug.Log("Finished");
-        }
+        }*/
 
         // Update is called once per frame
         void Update()
